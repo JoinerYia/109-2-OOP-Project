6c4531d580ad45491330e7222d7579eeea89d50e
108590006 <t108590006@ntut.org.tw>
2021-03-05 11:07:56 +0800

    某甲 練習七-甲 2

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,10 +1,18 @@
-#include "stdafx.h"
-#include <string>
-#include <iostream>
-
-using namespace System;
-
-int main(array<System::String ^> ^args)
-{
-	return 0;
-}
+#include "stdafx.h" 
+#include <string> 
+#include <iostream> 
+ 
+using namespace System; 
+ 
+bool Guess(int number) { 
+	return false; 
+} 
+ 
+int main(array<System::String ^> ^args) 
+{ 
+	int guess; 
+	do { 
+		std::cin >> guess; 
+	} while (!Guess(guess)); 
+	return 0; 
+}  
